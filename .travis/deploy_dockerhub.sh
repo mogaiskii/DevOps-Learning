1e9bedd0e22c578236c2b785e235a95b9b2cc3b1
mogaiskii <sani.mog@gmail.com>
2017-08-01 12:09:27 +0700

    dockerhub #5

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
+	TRAVIS_REPO_SLUG_LOW = $TRAVIS_REPO_SLUG | tr "[:upper:]" "[:lower:]"
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH" | tr "[:upper:]" "[:lower:]"
+    TRAVIS_REPO_SLUG_LOW = $TRAVIS_REPO_SLUG | tr "[:upper:]" "[:lower:]"
 fi
-TRAVIS_REPO_SLUG_LOW = $TRAVIS_REPO_SLUG | tr "[:upper:]" "[:lower:]"
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG_LOW:$TAG .
 docker push $TRAVIS_REPO_SLUG_LOW
